48504a626f36b53518010117702e083dacbe3dab
Edward Wo <edward.wo823@gmail.com>
2018-08-09 10:37:54 +0800

    remove cookieParser pkg

--- example/app.js
+++ example/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+//var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 // var fs = require('fs');
 var routes = require('./routes/index');
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // var privateKey = fs.readFileSync('ssl/key.pem', 'utf8');
